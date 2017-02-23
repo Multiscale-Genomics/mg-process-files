fe9f49d7695c2d403b66d27f462dc06346c81112
Mark McDowall <mark.mcdowall@gmail.com>
2017-02-23 08:56:03 +0000

    Minor changes for testing

--- scripts/test_hdf5.py
+++ scripts/test_hdf5.py
@@ -21,20 +21,20 @@ import random
 
 # 286sec ==> 0.286sec per query
 
-chr_list = [1,2,3,4,5,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,'X','Y']
-#chr_list = [1,10,11,12,13]
+#chr_list = [1,2,3,4,5,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,'X','Y']
+chr_list = [1,10,11,12,13]
 
-for i in range(10):
+for i in range(1000):
     chromosome = random.choice(chr_list)
     start = random.randint(1,45000000)
     end = start + 1000000
     h5r = hdf5_reader('test')
     
     # Mouse
-    #file_ids = h5r.get_regions('GCA_000001635.7', chromosome, start, end)
+    file_ids = h5r.get_regions('GCA_000001635.7', chromosome, start, end)
     
     # Human
-    file_ids = h5r.get_regions('GCA_000001405.22', chromosome, start, end)
+    #file_ids = h5r.get_regions('GCA_000001405.22', chromosome, start, end)
     
     #print(str(chromosome), str(start), str(end), str(file_ids))
     h5r.close()
