4cc7b6b15d0b46bf434a49c3a6cb9595defde246
Mark McDowall <mark.mcdowall@gmail.com>
2017-08-18 16:07:12 +0100

    Initial changes to adopt the new Tools API

--- process_bed.py
+++ process_bed.py
@@ -2,10 +2,10 @@
 
 """
 .. Copyright 2017 EMBL-European Bioinformatics Institute
- 
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at 
+   You may obtain a copy of the License at
 
        http://www.apache.org/licenses/LICENSE-2.0
 
@@ -16,27 +16,16 @@
    limitations under the License.
 """
 
-import argparse, gzip, shutil, shlex, subprocess, os.path, json
-from functools import wraps
+from __future__ import print_function
 
+import argparse
 
-from basic_modules import Tool, Workflow, Metadata
+from basic_modules.workflow import Workflow
+
 from dmp import dmp
 
-import tool
-import os
-
-try:
-    from pycompss.api.parameter import FILE_IN, FILE_OUT
-    from pycompss.api.task import task
-    from pycompss.api.constraint import constraint
-except ImportError :
-    print("[Warning] Cannot import \"pycompss\" API packages.")
-    print("          Using mock decorators.")
-    
-    from dummy_pycompss import *
-
-from tool import bed_indexer
+from tool.bed_indexer import bedIndexerTool
+from tool.bed_sorter import bedSortTool
 
 # ------------------------------------------------------------------------------
 
@@ -45,14 +34,14 @@ class process_bed(Workflow):
     Workflow to index BED formatted files within the Multiscale Genomics (MuG)
     Virtural Research Environment (VRE)
     """
-    
+
     def __init__(self):
         """
         Initialise the class
         """
-        
-    
-    def run(self, file_ids, metadata):
+
+
+    def run(self, input_files, metadata, output_files):
         """
         Main run function to index the BED files ready for use in the RESTful
         API. BED files are index in 2 different ways to allow for optimal data
@@ -62,93 +51,125 @@ class process_bed(Workflow):
         information at a given location. This is to help the REST clients make
         only the required calls to the relevant BED files rather than needing to
         pole all potential BED files.
-        
+
         Parameters
         ----------
-        files_ids : list
+        inpout_files : list
             List of file locations
         metadata : list
-        
+
         Returns
         -------
         outputfiles : list
             List of locations for the output BED and HDF5 files
         """
-        
-        bed_file = file_ids[0]
-        chrom_file = file_ids[1]
-        hdf5_file = file_ids[2]
+
+        bed_file = input_files[0]
+        chrom_file = input_files[1]
+        hdf5_file = input_files[2]
         assembly = metadata["assembly"]
-        file_id = metadata["file_id"]
         bed_type = metadata["bed_type"]
 
         meta_data = {
-            "file_id" : b_file,
             "bed_type" : bed_type,
             "assembly" : assembly
         }
-        
+
+        # Bed Sort
+        bst = bedSortTool()
+        bst_files, bst_meta = bst.run([bed_file], [], {})
+
         # Bed Indexer
-        b = bed_indexer.bedIndexerTool()
-        bb, h5_idx = b.run((bed_file, chrom_file, hdf5_file), meta_data)
-        
-        return (bb, h5_idx)
+        bit = bedIndexerTool()
+        bit_files, bit_meta = bit.run([bst_files[0], chrom_file, hdf5_file], [], meta_data)
+
+        return (bst_files + bit_files, bst_meta + bit_meta)
+
+# ------------------------------------------------------------------------------
+
+def main(input_files, output_files, input_metadata):
+    """
+    Main function
+    -------------
+
+    This function launches the app.
+    """
+
+    # import pprint  # Pretty print - module for dictionary fancy printing
+
+    # 1. Instantiate and launch the App
+    print("1. Instantiate and launch the App")
+    from apps.workflowapp import WorkflowApp
+    app = WorkflowApp()
+    result = app.launch(process_bed, input_files, input_metadata,
+                        output_files, {})
+
+    # 2. The App has finished
+    print("2. Execution finished")
+    print(result)
+    return result
 
 # ------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     import sys
-    import os
-    
+    sys._run_from_cmdl = True
+
     # Set up the command line parameters
-    parser = argparse.ArgumentParser(description="Index the bed file")
-    parser.add_argument("--assembly", help="Assembly")
-    parser.add_argument("--chrom", help="Matching chrom.size file")
-    parser.add_argument("--bed_file", help="Bed file to get indexed")
-    parser.add_argument("--h5_file", help="Location of HDF5 index file")
-    parser.add_argument("--bed_type", help="Type of Bed file bedN[+P]", default=None)
-    
+    PARSER = argparse.ArgumentParser(description="Index the bed file")
+    PARSER.add_argument("--assembly", help="Assembly")
+    PARSER.add_argument("--chrom", help="Matching chrom.size file")
+    PARSER.add_argument("--bed_file", help="Bed file to get indexed")
+    PARSER.add_argument("--h5_file", help="Location of HDF5 index file")
+    PARSER.add_argument("--bed_type", help="Type of Bed file bedN[+P]", default=None)
+
     # Get the matching parameters from the command line
-    args = parser.parse_args()
-    
-    assembly = args.assembly
-    chrom_size_file = args.chrom
-    bed_file = args.bed_file
-    hdf5_file = args.h5_file
-    bed_type = args.bed_type
-    
-    pb = process_bed()
-    
+    ARGS = PARSER.parse_args()
+
+    ASSEMBLY = ARGS.assembly
+    CHROM_SIZE_FILE = ARGS.chrom
+    BED_FILE = ARGS.bed_file
+    HDF5_FILE = ARGS.h5_file
+    BED_TYPE = ARGS.bed_type
+
+
+
     #
     # MuG Tool Steps
     # --------------
-    # 
+    #
     # 1. Create data files
     #    This should have already been done by the VRE - Potentially. If these
     #    Are ones that are present in the ENA then I would need to download them
-    
-    
-    #2. Register the data with the DMP
-    da = dmp(test=True)
-    
-    print(da.get_files_by_user("test"))
-    
-    cs_file = da.set_file("test", chrom_size_file, "tsv", "ChIP-seq", "", None, [], {"assembly" : assembly})
-    b_file = da.set_file("test", bed_file, "bed", "Assembly", "", None, [], {"assembly" : assembly})
-    h5_file = da.set_file("test", hdf5_file, "hdf5", "index", "", None, [cs_file, b_file], {"assembly" : assembly})
-    
-    print(da.get_files_by_user("test"))
-    
-    # 3. Instantiate and launch the App
-    #from basic_modules import WorkflowApp
-    #app = WorkflowApp()
-    #results = app.launch(process_bed, [b_file, cs_file, h5_file], {"assembly" : assembly})
 
-    metadata = {
-        "file_id" : b_file,
-        "bed_type" : bed_type,
-        "assembly" : assembly
+
+    #2. Register the data with the DMP
+    DM_HANDLER = dmp(test=True)
+
+    CS_FILE = DM_HANDLER.set_file(
+        "test", CHROM_SIZE_FILE, "tsv", "ChIP-seq", "", None, [],
+        {"assembly" : ASSEMBLY}
+    )
+    B_FILE = DM_HANDLER.set_file(
+        "test", BED_FILE, "bed", "Assembly", "", None, [],
+        {"assembly" : ASSEMBLY}
+    )
+    H5_FILE = DM_HANDLER.set_file(
+        "test", HDF5_FILE, "hdf5", "index", "", None, [CS_FILE, B_FILE],
+        {"assembly" : ASSEMBLY}
+    )
+
+    print(DM_HANDLER.get_files_by_user("test"))
+
+    # 3. Instantiate and launch the App
+
+    METADATA = {
+        "file_id" : B_FILE,
+        "bed_type" : BED_TYPE,
+        "assembly" : ASSEMBLY
     }
-    results = pb.run([bed_file, chrom_size_file, hdf5_file], metadata)
-    
-    print(da.get_files_by_user("test"))
+
+    RESULTS = main([BED_FILE, CHROM_SIZE_FILE, HDF5_FILE], [], METADATA)
+
+    print(RESULTS)
+    print(DM_HANDLER.get_files_by_user("test"))
