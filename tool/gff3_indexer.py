809306337fb39379186f153f0e3413510f379a34
Mark McDowall <mark.mcdowall@gmail.com>
2017-08-18 16:12:44 +0100

    Initial cleaning of the code based on pylint

--- tool/gff3_indexer.py
+++ tool/gff3_indexer.py
@@ -1,9 +1,9 @@
 """
 .. Copyright 2017 EMBL-European Bioinformatics Institute
- 
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at 
+   You may obtain a copy of the License at
 
        http://www.apache.org/licenses/LICENSE-2.0
 
@@ -27,7 +27,7 @@ try:
 except ImportError :
     print("[Warning] Cannot import \"pycompss\" API packages.")
     print("          Using mock decorators.")
-    
+
     from dummy_pycompss import *
 
 from basic_modules.metadata import Metadata
@@ -39,14 +39,14 @@ class gff3IndexerTool(Tool):
     """
     Tool for running indexers over a WIG file for use in the RESTful API
     """
-    
+
     def __init__(self):
         """
         Init function
         """
         print "GFF3 File Indexer"
-    
-    
+
+
     def gff3Sorter(self, file_gff3):
         """
         Sorts the GFF3 file in preparation for compression and indexing
@@ -76,7 +76,7 @@ class gff3IndexerTool(Tool):
             sorted.wait()
 
         return file_sorted_gff3
-        
+
 
     @task(file_sorted_gff3=FILE_IN, file_sorted_gz_gff3=FILE_OUT, file_gff3_tbi=FILE_OUT)
     def gff32tabix(self, file_sorted_gff3, file_sorted_gz_gff3, file_gff3_tbi):
@@ -99,7 +99,7 @@ class gff3IndexerTool(Tool):
         -------
         .. code-block:: python
            :linenos:
-           
+
            if not self.gff32tabix(self, file_sorted_gff3, gz_file, tbi_file):
                output_metadata.set_exception(
                    Exception(
@@ -113,12 +113,12 @@ class gff3IndexerTool(Tool):
     def gff32hdf5(self, file_id, assembly, file_sorted_gff3, file_hdf5):
         """
         GFF3 to HDF5 converter
-        
+
         Loads the GFF3 file into the HDF5 index file that gets used by the REST
         API to determine if there are files that have data in a given region.
         Overlapping regions are condensed into a single feature block rather
         than maintaining all of the detail of the original bed file.
-        
+
         Parameters
         ----------
         file_id : str
@@ -131,28 +131,28 @@ class gff3IndexerTool(Tool):
             Location of the sorted GFF3 file
         file_hdf5 : str
             Location of the HDF5 index file
-        
+
         Example
         -------
         .. code-block:: python
            :linenos:
-           
+
            if not self.gff32hdf5(file_id, assembly, bed_file, hdf5_file):
                output_metadata.set_exception(
                    Exception(
                        "gff32hdf5: Could not process files {}, {}.".format(*input_files)))
-        
+
         """
         MAX_FILES = 1024
         MAX_CHROMOSOMES = 1024
         MAX_CHROMOSOME_SIZE = 2000000000
-        
+
         f = h5py.File(file_hdf5, "a")
-        
+
         if str(assembly) in f:
             grp  = f[str(assembly)]
             meta = f['meta']
-            
+
             dset = grp['data']
             fset = grp['files']
             cset = grp['chromosomes']
@@ -161,25 +161,25 @@ class gff3IndexerTool(Tool):
                 file_idx.append(file_id)
                 dset.resize((dset.shape[0], dset.shape[1]+1, MAX_CHROMOSOME_SIZE))
             chrom_idx = [c for c in cset if c != '']
-                
+
         else:
             # Create the initial dataset with minimum values
             grp    = f.create_group(str(assembly))
             meta   = f.create_group('meta')
-            
+
             dtf = h5py.special_dtype(vlen=str)
             dtc = h5py.special_dtype(vlen=str)
             fset = grp.create_dataset('files', (MAX_FILES,), dtype=dtf)
             cset = grp.create_dataset('chromosomes', (MAX_CHROMOSOMES,), dtype=dtc)
-            
+
             file_idx  = [file_id]
             chrom_idx = []
-            
+
             dset = grp.create_dataset('data', (0, 1, MAX_CHROMOSOME_SIZE), maxshape=(MAX_CHROMOSOMES, MAX_FILES, MAX_CHROMOSOME_SIZE), dtype='bool', chunks=True, compression="gzip")
-        
+
         # Save the list of files
         fset[0:len(file_idx)] = file_idx
-        
+
         file_chrom_count = 0
 
         dnp = np.zeros([MAX_CHROMOSOME_SIZE], dtype='bool')
@@ -187,35 +187,35 @@ class gff3IndexerTool(Tool):
         previous_chrom = ''
         previous_start = 0
         previous_end   = 0
-        
+
         loaded = False
-        
+
         fi = open(file_sorted_gff3, 'r')
         for line in fi:
             line = line.strip()
             l = line.split("\t")
-            
+
             c = str(l[0])
             s = int(l[3])
             e = int(l[4])
-            
+
             loaded = False
-            
+
             if c != previous_chrom and previous_chrom != '':
                 file_chrom_count += 1
                 if previous_chrom not in chrom_idx:
                     chrom_idx.append(previous_chrom)
                     cset[0:len(chrom_idx)] = chrom_idx
                     dset.resize((dset.shape[0]+1, dset.shape[1], MAX_CHROMOSOME_SIZE))
-                
+
                 dset[chrom_idx.index(previous_chrom), file_idx.index(file_id), :] = dnp
                 loaded = True
-                
+
                 if file_chrom_count == 5:
                     break
-                
+
                 dnp = np.zeros([MAX_CHROMOSOME_SIZE], dtype='bool')
-            
+
             previous_chrom = c
             dnp[s:e+1] = 1
 
@@ -224,20 +224,20 @@ class gff3IndexerTool(Tool):
                 chrom_idx.append(c)
                 cset[0:len(chrom_idx)] = chrom_idx
                 dset.resize((dset.shape[0]+1, dset.shape[1], MAX_CHROMOSOME_SIZE))
-            
+
             dset[chrom_idx.index(previous_chrom), file_idx.index(file_id), :] = dnp
-        
+
         f.close()
         fi.close()
-        
+
         return True
-    
-    
+
+
     def run(self, input_files, metadata):
         """
         Function to run the BED file sorter and indexer so that the files can
         get searched as part of the REST API
-        
+
         Parameters
         ----------
         input_files : list
@@ -250,7 +250,7 @@ class gff3IndexerTool(Tool):
                 file_id used to identify the original bed file
             assembly : str
                 Genome assembly accession
-        
+
         Returns
         -------
         list
@@ -260,26 +260,26 @@ class gff3IndexerTool(Tool):
                 Location of the Tabix index file
             hdf5_file : str
                 Location of the HDF5 index file
-        
+
         Example
         -------
         .. code-block:: python
            :linenos:
-           
+
            import tool
-           
+
            # Bed Indexer
            g = tool.gff3IndexerTool(self.configuration)
            gff3_files, gff3_meta = g.run((gff3_file_id, hdf5_file_id), {'file_id' : file_id, 'assembly' : assembly})
         """
         gff3_file   = input_files[0]
         hdf5_file  = input_files[2]
-        
+
         gz_file = gff3_file + '.gz'
         tbi_file = gz_file + '.tbi'
-        
+
         assembly = meta_data['assembly']
-        
+
         file_sorted_gff3 = self.gff3Sorter(gff3_file)
 
         # handle error
@@ -289,12 +289,12 @@ class gff3IndexerTool(Tool):
                     "gff32tabix: Could not process files {}, {}.".format(*input_files)))
             gz_file  = None
             tbi_file = None
-        
+
         if not self.gff32hdf5(file_id, assembly, file_sorted_gff3, hdf5_file):
             output_metadata.set_exception(
                 Exception(
                     "gff32hdf5: Could not process files {}, {}.".format(*input_files)))
-        
+
         return ([gz_file, tbi_file, hdf5_file], [output_metadata])
 
 # ------------------------------------------------------------------------------
--- tool/wig_indexer.py
+++ tool/wig_indexer.py
@@ -1,9 +1,9 @@
 """
 .. Copyright 2017 EMBL-European Bioinformatics Institute
- 
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at 
+   You may obtain a copy of the License at
 
        http://www.apache.org/licenses/LICENSE-2.0
 
@@ -27,7 +27,7 @@ try:
 except ImportError :
     print("[Warning] Cannot import \"pycompss\" API packages.")
     print("          Using mock decorators.")
-    
+
     from dummy_pycompss import *
 
 from basic_modules.metadata import Metadata
@@ -39,23 +39,23 @@ class wigIndexerTool(Tool):
     """
     Tool for running indexers over a WIG file for use in the RESTful API
     """
-    
+
     def __init__(self):
         """
         Init function
         """
         print "WIG File Indexer"
-    
-    
+
+
     @task(file_wig=FILE_IN, file_chrom=FILE_IN, file_bw=FILE_OUT)
     def wig2bigwig(self, file_wig, file_chrom, file_bw):
         """
         WIG to BigWig converter
-        
+
         This uses the ``wigToBigWig`` program binary provided at
         http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/
         to perform the conversion from WIG to BigWig.
-        
+
         Parameters
         ----------
         file_wig : str
@@ -64,35 +64,35 @@ class wigIndexerTool(Tool):
             Location of the chrom.size file
         file_bw : str
             Location of the bigWig file
-        
+
         Example
         -------
         .. code-block:: python
            :linenos:
-           
+
            if not self.wig2bigwig(wig_file, chrom_file, bw_file):
                output_metadata.set_exception(
                    Exception(
                        "wig2bigWig: Could not process files {}, {}.".format(*input_files)))
-        
+
         """
         command_line = 'wigToBigWig ' + file_wig + ' ' + file_chrom + ' ' + file_bw
         args = shlex.split(command_line)
         p = subprocess.Popen(args)
         p.wait()
         return True
-    
-    
+
+
     @task(file_id=IN, assembly=IN, file_wig=FILE_IN, file_hdf5=FILE_INOUT)
     def wig2hdf5(self, file_id, assembly, file_wig, file_hdf5):
         """
         WIG to HDF5 converter
-        
+
         Loads the WIG file into the HDF5 index file that gets used by the REST
         API to determine if there are files that have data in a given region.
         Overlapping regions are condensed into a single feature block rather
         than maintaining all of the detail of the original WIG file.
-        
+
         Parameters
         ----------
         file_id : str
@@ -105,28 +105,28 @@ class wigIndexerTool(Tool):
             Location of the wig file
         file_hdf5 : str
             Location of the HDF5 index file
-        
+
         Example
         -------
         .. code-block:: python
            :linenos:
-           
+
            if not self.wig2hdf5(file_id, assembly, wig_file, hdf5_file):
                output_metadata.set_exception(
                    Exception(
                        "wig2hdf5: Could not process files {}, {}.".format(*input_files)))
-        
+
         """
         MAX_FILES = 1024
         MAX_CHROMOSOMES = 1024
         MAX_CHROMOSOME_SIZE = 2000000000
-        
+
         f = h5py.File(file_hdf5, "a")
-        
+
         if str(assembly) in f:
             grp  = f[str(assembly)]
             meta = f['meta']
-            
+
             dset = grp['data']
             fset = grp['files']
             cset = grp['chromosomes']
@@ -135,34 +135,34 @@ class wigIndexerTool(Tool):
                 file_idx.append(file_id)
                 dset.resize((dset.shape[0], dset.shape[1]+1, MAX_CHROMOSOME_SIZE))
             chrom_idx = [c for c in cset if c != '']
-                
+
         else:
             # Create the initial dataset with minimum values
             grp    = f.create_group(str(assembly))
             meta   = f.create_group('meta')
-            
+
             dtf = h5py.special_dtype(vlen=str)
             dtc = h5py.special_dtype(vlen=str)
             fset = grp.create_dataset('files', (MAX_FILES,), dtype=dtf)
             cset = grp.create_dataset('chromosomes', (MAX_CHROMOSOMES,), dtype=dtc)
-            
+
             file_idx  = [file_id]
             chrom_idx = []
-            
+
             dset = grp.create_dataset(
                 'data1', (0, 1, MAX_CHROMOSOME_SIZE),
                 maxshape=(MAX_CHROMOSOMES, MAX_FILES, MAX_CHROMOSOME_SIZE),
                 dtype='bool', chunks=True, compression="gzip")
-        
+
         # Save the list of files
         fset[0:len(file_idx)] = file_idx
-        
+
         file_chrom_count = 0
 
         dnp = np.zeros([MAX_CHROMOSOME_SIZE], dtype='bool')
 
         loaded = False
-        
+
         fi = open(file_wig, 'r')
         start = 0
         span  = 1
@@ -181,15 +181,15 @@ class wigIndexerTool(Tool):
                 step  = 1
                 previous_chrom = ''
                 step_type = 'variable'
-                
+
                 l = line.split(' ')
-                
+
                 if l[0][0:9] == 'fixedStep':
                     step_type = 'fixed'
-                
+
                 if previous_chrom != '':
                     dnp[previous_start:previous_end+1] = 1
-                
+
                 chrom = ''
                 for kv in l[1:]:
                     kv.strip()
@@ -204,21 +204,21 @@ class wigIndexerTool(Tool):
                         step = int(v)
                     elif k == 'chrom':
                         chrom = v
-                
+
                 file_chrom_count += 1
                 if previous_chrom != '' and previous_chrom != chrom:
                     if previous_chrom not in chrom_idx:
                         chrom_idx.append(previous_chrom)
                         cset[0:len(chrom_idx)] = chrom_idx
                         dset.resize((dset.shape[0]+1, dset.shape[1], MAX_CHROMOSOME_SIZE))
-                    
+
                     dset[chrom_idx.index(previous_chrom), file_idx.index(file_id), :] += dnp
-                    
+
                     dnp = np.zeros([MAX_CHROMOSOME_SIZE], dtype='bool')
                     loaded = True
-                
+
                 previous_chrom = chrom
-                
+
             else:
                 loaded = False
                 #print(chrom, str(start), str(step), str(span))
@@ -239,9 +239,9 @@ class wigIndexerTool(Tool):
                                 dnp[previous_start:previous_end+1] = 1
                                 previous_start = start
                                 previous_end   = start + span - 1
-                    
+
                     start += step
-                    
+
                 elif step_type == 'variable':
                     l = line.split("\t")
                     if float(l[1]) == 0.0:
@@ -260,26 +260,26 @@ class wigIndexerTool(Tool):
                                 dnp[previous_start:previous_end+1] = 1
                                 previous_start = int(l[0])
                                 previous_end   = int(l[0]) + span - 1
-        
+
         if loaded == False:
             if previous_chrom not in chrom_idx:
                 chrom_idx.append(c)
                 cset[0:len(chrom_idx)] = chrom_idx
                 dset.resize((dset.shape[0]+1, dset.shape[1], MAX_CHROMOSOME_SIZE))
-            
+
             dset[chrom_idx.index(previous_chrom), file_idx.index(file_id), :] = dnp
-        
+
         f.close()
         fi.close()
-        
+
         return True
-    
-    
+
+
     def run(self, input_files, metadata):
         """
         Function to run the WIG file sorter and indexer so that the files can
         get searched as part of the REST API
-        
+
         Parameters
         ----------
         input_files : list
@@ -294,7 +294,7 @@ class wigIndexerTool(Tool):
                 file_id used to identify the original wig file
             assembly : str
                 Genome assembly accession
-        
+
         Returns
         -------
         list
@@ -302,14 +302,14 @@ class wigIndexerTool(Tool):
                 Location of the BigWig file
             hdf5_file : str
                 Location of the HDF5 index file
-        
+
         Example
         -------
         .. code-block:: python
            :linenos:
-           
+
            import tool
-           
+
            # WIG Indexer
            w = tool.bedIndexerTool(self.configuration)
            wi, wm = w.run((wig_file_id, chrom_file_id, hdf5_file_id), {'file_id' : file_id, 'assembly' : assembly})
@@ -317,25 +317,25 @@ class wigIndexerTool(Tool):
         wig_file   = input_files[0]
         chrom_file = input_files[1]
         hdf5_file  = input_files[2]
-        
+
         bw_name = wig_file.split("/")
         bw_name[-1].replace('.wig', '.bw')
         bw_file = '/'.join(bw_name)
-        
+
         file_id = metadata['file_id']
         assembly = metadata['assembly']
-        
+
         # handle error
         if not self.wig2bigwig(wig_file, chrom_file, bw_file):
             output_metadata.set_exception(
                 Exception(
                     "wig2bigwig: Could not process files {}, {}.".format(*input_files)))
-        
+
         if not self.wig2hdf5(file_id, assembly, wig_file, hdf5_file):
             output_metadata.set_exception(
                 Exception(
                     "wig2hdf5: Could not process files {}, {}.".format(*input_files)))
-        
+
         return ([bw_file, hdf5_file], [output_metadata])
 
 # ------------------------------------------------------------------------------
