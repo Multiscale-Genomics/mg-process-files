98282385e058dfc92b5ff8c8464ea0401ce4b622
Mark McDowall <mark.mcdowall@gmail.com>
2018-07-06 16:38:00 +0100

    Fix to the testd

--- scripts/travis/test_runner.sh
+++ scripts/travis/test_runner.sh
@@ -15,22 +15,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-pytest -s tests/test_bed_functions.py
+pytest -s mg_process_files/tests/test_bed_functions.py
 tc=$?
 rc=$(($rc + $tc))
 ./tidy_data.sh
 
-pytest -s tests/test_gff3_functions.py
+pytest -s mg_process_files/tests/test_gff3_functions.py
 tc=$?
 rc=$(($rc + $tc))
 ./tidy_data.sh
 
-pytest -s tests/test_wig_functions.py
+pytest -s mg_process_files/tests/test_wig_functions.py
 tc=$?
 rc=$(($rc + $tc))
 ./tidy_data.sh
 
-pytest -s tests/test_json3d_functions.py
+pytest -s mg_process_files/tests/test_json3d_functions.py
 tc=$?
 rc=$(($rc + $tc))
 ./tidy_data.sh
